db2e1d2f0a839a09e0a8a8c7d8c4be60c86b022a
Wesley <wesley-chapado@hotmail.com>
2025-02-10 23:48:16 -0300

    Ajuste de import.

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ import { Hono } from 'hono'
 import { listUsers } from "./src/app/features/user/controllers/get_users_controller.ts";
 import { addUser } from "./src/app/features/user/controllers/create_users_controller.ts";
 import { authGuard } from "./src/app/auth/guard/auth_guard.ts";
-import { downloadAudio, getVideo } from "./src/app/features/video/controller/get_video_info_controller.ts";
+import { getVideo } from "./src/app/features/video/controller/get_video_info_controller.ts";
+import { downloadAudio } from "./src/app/features/video/controller/download_audio_controller.ts";
 
 const app = new Hono()
 
